90dc44acaf4073bcf35358180de7fe5e83f66287
Jurand76 <jurand@interia.pl>
2023-06-20 23:22:01 +0200

    feat: day74 solution

---
+++ day74_modules.py
@@ -0,0 +1,6 @@
+import text
+
+text_to_convert = input("Wpisz tekst: ")
+print("Tekst po konwersji - male litery: ", text.small_letters(text_to_convert))
+print("Tekst po konwersji - duze litery: ", text.large_letters(text_to_convert))
+
---
+++ text.py
@@ -0,0 +1,7 @@
+# small_letters - change text to small letters only
+
+def small_letters(s):
+    return(s.lower())
+
+def large_letters(s):
+    return(s.upper())
